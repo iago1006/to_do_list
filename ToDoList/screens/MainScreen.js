e4d20e840ad691447f25bd29e256372d343a7b66
Iago <iago.carbajal.rojas@gmail.com>
2023-10-02 00:46:04 -0500

    Listas visibles

--- ToDoList/screens/MainScreen.js
+++ ToDoList/screens/MainScreen.js
@@ -1,30 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, ImageBackground } from 'react-native';
+import { View, ActivityIndicator, Text, TouchableOpacity, FlatList, StyleSheet, ImageBackground } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
-import { removeAuthToken } from '../auth';
+import { removeAuthToken, getAuthToken } from '../auth';
+import axios from 'axios';
 
 const MainScreen = ({ navigation }) => {
   const [taskLists, setTaskLists] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simula la obtención de las listas de tareas del usuario desde tu API o base de datos.
-    const fakeTaskLists = [
-      { id: 1, name: 'Lista de Tareas 1' },
-      { id: 2, name: 'Lista de Tareas 2' },
-      { id: 3, name: 'Lista de Tareas 3' },
-    ];
-    setTaskLists(fakeTaskLists);
-  }, []);
+    const fetchTaskLists = async () => {
+      const authToken = await getAuthToken();
+
+      if (!authToken) {
+        navigation.navigate('Login');
+        return;
+      }
+
+      const apiUrl = 'http://192.168.0.14:3000/lists';
+
+      try {
+        const response = await axios.get(apiUrl, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+
+        setTaskLists(response.data);
+      } catch (error) {
+        console.error('Error al obtener las listas de tareas:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTaskLists();
+  }, [navigation]);
 
   const handleLogout = async () => {
     await removeAuthToken();
     navigation.navigate('Login');
   };
 
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={styles.loadingText}>Cargando...</Text>
+      </View>
+    );
+  }  
+
   return (
     <ImageBackground
-      source={require('../images/background.jpg')} // Ruta de la imagen de fondo
+      source={require('../images/background.jpg')}
       style={styles.container}
     >
       <View style={styles.header}>
@@ -35,7 +65,7 @@ const MainScreen = ({ navigation }) => {
       </View>
       <FlatList
         data={taskLists}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item.list_id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity
             style={styles.listItem}
@@ -54,8 +84,8 @@ const MainScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    resizeMode: 'cover', // Ajustar la imagen al tamaño de la pantalla
-    justifyContent: 'center', // Centrar elementos verticalmente
+    resizeMode: 'cover',
+    justifyContent: 'center',
     paddingTop: 50,
     paddingHorizontal: 30,
   },
@@ -63,7 +93,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)', // Fondo blanco semi-transparente
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
     padding: 16,
     borderRadius: 8,
   },
@@ -82,6 +112,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255, 255, 255, 0.7)',
     marginBottom: 8,
     borderRadius: 8,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
