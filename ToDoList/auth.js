de2eff3048be96a9c1aff5f7b61060082c9e6dcf
iago1006 <iago.carbajal.rojas@gmail.com>
2023-12-08 15:45:47 -0500

    Final v1

--- ToDoList/auth.js
+++ ToDoList/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_URL = 'http://192.168.0.124:3000';
+export const API_URL = 'https://apitask-bp8t.onrender.com/';
 
 // Función para almacenar el token de autenticación en el almacenamiento local
 export const storeAuthToken = async (token) => {
