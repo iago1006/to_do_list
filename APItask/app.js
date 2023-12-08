0809efd8ebcba21f64fd5fad0c1ec0eddb037742
edumarce03 <eduardomarce2003@gmail.com>
2023-12-08 15:36:43 -0500

    Últimos cambios

--- APItask/app.js
+++ APItask/app.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
 });
 
 // Conecta a la base de datos
